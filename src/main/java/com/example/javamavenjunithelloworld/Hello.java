0084492c7031651e4896089ecbe5e6d185be8f02
akshaytiwari29 <41627550+akshaytiwari29@users.noreply.github.com>
2019-01-05 09:02:23 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -40,23 +40,7 @@ public class Hello {
         }
     }
     
-    private static double volume(String solidom, double alturam, double areaBasem, double raiom) {
-double vol;
-    if (solidom.equalsIgnoreCase("esfera"){
-        vol=(4.0/3)*Math.pi*Math.pow(raiom,3);
-    }
-    else {
-        if (solidom.equalsIgnoreCase("cilindro") {
-            vol=Math.pi*Math.pow(raiom,2)*alturam;
-        }
-        else {
-            vol=(1.0/3)*Math.pi*Math.pow(raiom,2)*alturam;
-        }
-    }
-    return vol;
-}
-    
-    
+  
     
 
 
