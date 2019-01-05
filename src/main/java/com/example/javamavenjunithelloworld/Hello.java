a789e2f4d593e58989b26b8f8dae8d16c23f3c79
akshaytiwari29 <41627550+akshaytiwari29@users.noreply.github.com>
2019-01-05 09:06:56 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -40,6 +40,15 @@ public class Hello {
         }
     }
     
+    int minval(int *A, int n) {
+  int currmin;
+
+  for (int i=0; i<n; i++)
+    if (A[i] < currmin)
+      currmin = A[i];
+  return currmin;
+}
+    
   
     
 
